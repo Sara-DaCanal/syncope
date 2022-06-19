7b42bc0e567468791b67893cbd89c6a05f22035d
Sara-DaCanal <s.saradacanal@gmail.com>
2022-06-19 23:15:48 +0200

    test encryptor con pit

--- core/spring/src/test/java/org/apache/syncope/core/spring/security/TestEncryptorPIT.java
+++ core/spring/src/test/java/org/apache/syncope/core/spring/security/TestEncryptorPIT.java
@@ -2,13 +2,19 @@ package org.apache.syncope.core.spring.security;
 
 import org.apache.syncope.common.lib.types.CipherAlgorithm;
 import org.apache.syncope.core.spring.ApplicationContextProvider;
-import org.junit.*;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
-import org.mockito.*;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 
-import javax.crypto.*;
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 import java.io.UnsupportedEncodingException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
@@ -16,27 +22,27 @@ import java.util.Arrays;
 import java.util.Collection;
 
 @RunWith(Parameterized.class)
-public class TestEncryptor{
+public class TestEncryptorPIT{
     private  static Encryptor encryptor;
     private String value;
     private CipherAlgorithm cipherAlgorithm;
     private String encoded;
     static MockedStatic<ApplicationContextProvider> util;
 
-    
-    public TestEncryptor(string value, CipherAlgorithm cipherAlgorithm){
+
+    public TestEncryptorPIT(string value, CipherAlgorithm cipherAlgorithm){
         configure(value, cipherAlgorithm);
     }
 
-   @BeforeClass
+    @BeforeClass
     public static void setUp(){
         DefaultListableBeanFactory factory=new DefaultListableBeanFactory();
 
         factory.registerSingleton("securityProperties", new SecurityProperties());
 
-           util = Mockito.mockStatic(ApplicationContextProvider.class);
-            util.when(ApplicationContextProvider::getBeanFactory).thenReturn(factory);
-            util.when(ApplicationContextProvider::getApplicationContext).thenReturn(new DummyConfigurableApplicationContext(factory));
+        util = Mockito.mockStatic(ApplicationContextProvider.class);
+        util.when(ApplicationContextProvider::getBeanFactory).thenReturn(factory);
+        util.when(ApplicationContextProvider::getApplicationContext).thenReturn(new DummyConfigurableApplicationContext(factory));
         encryptor = Encryptor.getInstance();
     }
 
@@ -49,8 +55,6 @@ public class TestEncryptor{
             case VOID:
                 this.value="";
                 break;
-            case NULL:
-                this.value=null;
         }
     }
 
@@ -58,41 +62,29 @@ public class TestEncryptor{
     public static Collection parameters(){
         return Arrays.asList(new Object[][]{
                 {string.VALID, CipherAlgorithm.AES},
-                {string.NULL, CipherAlgorithm.AES},
                 {string.VOID, CipherAlgorithm.AES},
                 {string.VALID, CipherAlgorithm.SHA},
-                {string.NULL, CipherAlgorithm.SHA},
                 {string.VOID, CipherAlgorithm.SHA},
                 {string.VALID, CipherAlgorithm.SHA1},
-                {string.NULL, CipherAlgorithm.SHA1},
                 {string.VOID, CipherAlgorithm.SHA1},
                 {string.VALID, CipherAlgorithm.SHA256},
-                {string.NULL, CipherAlgorithm.SHA256},
                 {string.VOID, CipherAlgorithm.SHA256},
                 {string.VALID, CipherAlgorithm.SHA512},
-                {string.NULL, CipherAlgorithm.SHA512},
                 {string.VOID, CipherAlgorithm.SHA512},
                 {string.VALID, CipherAlgorithm.SMD5},
-                {string.NULL, CipherAlgorithm.SMD5},
                 {string.VOID, CipherAlgorithm.SMD5},
                 {string.VALID, CipherAlgorithm.SSHA},
                 {string.VOID, CipherAlgorithm.SSHA},
-                {string.NULL, CipherAlgorithm.SSHA},
                 {string.VALID, CipherAlgorithm.SSHA1},
-                {string.NULL, CipherAlgorithm.SSHA1},
                 {string.VOID, CipherAlgorithm.SSHA1},
                 {string.VALID, CipherAlgorithm.SSHA256},
-                {string.NULL, CipherAlgorithm.SSHA256},
                 {string.VOID, CipherAlgorithm.SSHA256},
                 {string.VALID, CipherAlgorithm.SSHA512},
                 {string.VOID, CipherAlgorithm.SSHA512},
-                {string.NULL, CipherAlgorithm.SSHA512},
                 {string.VALID, CipherAlgorithm.BCRYPT},
                 {string.VOID, CipherAlgorithm.BCRYPT},
-                {string.NULL, CipherAlgorithm.BCRYPT},
                 {string.VALID, null},
-                {string.NULL, null},
-                {string.VOID, null},
+                {string.VOID, null}
         });
     }
 
@@ -101,13 +93,13 @@ public class TestEncryptor{
     @Test
     public void test() {
         try {
-
             encoded = encryptor.encode(value, cipherAlgorithm);
-            if(value==null) Assert.assertNull(encoded);
-            else Assert.assertTrue(encryptor.verify(value, cipherAlgorithm, encoded));
+            if(cipherAlgorithm==null) Assert.assertEquals(encoded, encryptor.encode(value,CipherAlgorithm.AES));
+            else if(cipherAlgorithm.isSalted() || cipherAlgorithm.equals(CipherAlgorithm.BCRYPT)) Assert.assertNotEquals(encoded, encryptor.encode(value, cipherAlgorithm));
+            else Assert.assertEquals(encoded, encryptor.encode(value,cipherAlgorithm));
         } catch (UnsupportedEncodingException | NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException |
                 IllegalBlockSizeException | BadPaddingException e) {
-           Assert.fail();
+            Assert.fail();
         }
     }
 
@@ -117,7 +109,8 @@ public class TestEncryptor{
     }
 
     private enum string{
-        VALID, VOID, NULL
+        VALID, VOID
     }
 
 }
+
